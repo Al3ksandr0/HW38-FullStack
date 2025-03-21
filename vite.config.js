a5010c82b1b2fd35b1088522961836a1a157a551
Sasha <79007185+Al3ksandr0@users.noreply.github.com>
2025-03-21 18:09:28 +0200

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  base: "/HW39-1-FullStack/", //тут менять имя реп
-  plugins: [react()],
-});
